e2d4dd62c94d475497877c809bd8048d72548f2c
Marcel Göhn <marcelgoehn@googlemail.com>
2026-03-15 14:02:43 +0100

    feat: added csrf token method decorator to add a csrf token when logging in

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_str
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.utils.decorators import method_decorator
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -132,6 +134,7 @@ class LoginView(TokenObtainPairView):
 
     serializer_class = CustomTokenObtainPairSerializer
 
+    @method_decorator(ensure_csrf_cookie)
     def post(self, request, *args, **kwargs):
         """
         Validates the email and password. After that it log's the user in and
